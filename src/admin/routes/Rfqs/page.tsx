61d35920229cc198d784e7ede620270205740754
Govind Diwakar <sgf@sourcegoodfood.com>
2024-05-05 08:19:06 +0000

    fix: admin routes

--- src/admin/routes/Rfqs/page.tsx
+++ src/admin/routes/Rfqs/page.tsx
@@ -1,25 +1,28 @@
 import { RouteConfig } from "@medusajs/admin";
 import React, { useState } from "react";
-
-import AuctionViewOngoing from "../../widgets/auction-view-ongoing";
+import AuctionViewOnGoing from "../../widgets/auction-view-ongoing";
 
 const Bids = (): React.ReactElement => {
-    return AuctionViewOngoing({
-        notify: {
-            success: function (title: string, message: string): void {
-                console.log(`${title}: ${message} `);
-            },
-            error: function (title: string, message: string): void {
-                console.error(`${title}: ${message} `);
-            },
-            warn: function (title: string, message: string): void {
-                console.warn(`${title}: ${message} `);
-            },
-            info: function (title: string, message: string): void {
-                console.info(`${title}: ${message} `);
-            }
-        }
-    });
+    return (
+        <>
+            <AuctionViewOnGoing
+                notify={{
+                    success: function (title: string, message: string): void {
+                        console.log(`${title} : ${message}`);
+                    },
+                    error: function (title: string, message: string): void {
+                        console.error(`${title} : ${message}`);
+                    },
+                    warn: function (title: string, message: string): void {
+                        console.warn(`${title} : ${message}`);
+                    },
+                    info: function (title: string, message: string): void {
+                        console.info(`${title} : ${message}`);
+                    }
+                }}
+            />
+        </>
+    );
 };
 export default Bids;
 
