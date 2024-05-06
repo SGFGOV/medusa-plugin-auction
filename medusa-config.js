3e691c0035b472e69546d29dd9bdc24904fe6bdf
Govind Diwakar <sgf@sourcegoodfood.com>
2024-05-06 04:40:27 +0000

    fix: page routers

--- medusa-config.js
+++ medusa-config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const dotenv = require("dotenv");
 
 let ENV_FILE_NAME = "";
--- src/admin/routes/auctions/page.tsx
+++ src/admin/routes/auctions/page.tsx
@@ -1,6 +1,8 @@
 import { RouteConfig } from "@medusajs/admin";
 import AuctionViewOnGoing from "../../components/auction/auction-view-ongoing";
 import AuctionBids from "../../components/auction/auction-bids";
+import { Puzzle } from "@medusajs/icons";
+import { Container } from "@medusajs/ui";
 
 const notificationHandler = {
     success: function (title: string, message: string): void {
@@ -19,16 +21,17 @@ const notificationHandler = {
 
 const Auctions = (): React.ReactElement => {
     return (
-        <>
+        <Container title="My Auctions">
             <AuctionViewOnGoing notify={notificationHandler} />
             <AuctionBids notify={notificationHandler} />
-        </>
+        </Container>
     );
 };
 export default Auctions;
 
 export const config: RouteConfig = {
     link: {
-        label: process.env.MEDUSA_ADMIN_AUCTION_NAME ?? "Reverse Auction"
+        label: process.env.MEDUSA_ADMIN_AUCTION_NAME ?? "Reverse Auction",
+        icon: Puzzle
     }
 };
